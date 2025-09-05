da811e2396404cd42736c756677a62112bcf42e9
shiv-2005-eng <shivamkumar5263727@gmail.com>
2025-09-05 20:10:05 +0530

    Delete start-server.bat

--- start-server.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Gemini Ally Development Server...
-echo.
-echo The application will be available at: http://localhost:8080
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-npm run dev
